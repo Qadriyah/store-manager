642bf4d5c9b7e3d6cd4cf06b27a8ff40eef1258e
Qadriyah <b.alzawad@gmail.com>
2018-10-11 14:49:24 +0300

    Feat: The attendant should be able to add items to the cart and create a sales record
    This commit delivers the attendants dashboard and a page where the
    attendant can add items to the shopping cart and create a sales
    record.
    [Delivers #161077283]

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,3 +1,4 @@
+// onsubmit event handler
 onSubmit = () => {
   const name = document.getElementById('name').value;
   const password = document.getElementById('password').value;
@@ -10,3 +11,28 @@ onSubmit = () => {
     return false;
   }
 };
+
+openDrawer = () => {
+  document.getElementById('drawer').style =
+    'width: 150px; position: absolute; top: 10px; z-index: 1;';
+  document.getElementById('open').style.display = 'none';
+  document.getElementById('close').style.display = 'block';
+};
+
+closeDrawer = () => {
+  document.getElementById('drawer').style = 'width: 0;';
+  document.getElementById('close').style.display = 'none';
+  document.getElementById('open').style.display = 'block';
+};
+
+window.addEventListener('resize', () => {
+  if (this.window.innerWidth > 615) {
+    document.getElementById('drawer').style = 'width: 150px;';
+    document.getElementById('close').style.display = 'none';
+    document.getElementById('open').style.display = 'none';
+  } else {
+    document.getElementById('drawer').style = 'width: 0;';
+    document.getElementById('open').style.display = 'block';
+    document.getElementById('close').style.display = 'none';
+  }
+});
