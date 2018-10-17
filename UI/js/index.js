8cb86b57725aca76af2d2ae8fb62b8edf48e3e41
Qadriyah <b.alzawad@gmail.com>
2018-10-17 10:15:57 +0300

    Bg: Remove duplicatye javascript code
    This commit removes duplicate javascript code as reported by codeclimate
    [Fixes #161277003]

--- UI/js/index.js
+++ UI/js/index.js
@@ -13,31 +13,43 @@ window.onload = () => {
     });
   }
 
-  openDrawer = () => {
-    document.getElementById('drawer').style =
-      'width: 150px; position: absolute; top: 10px; z-index: 1;';
-    document.getElementById('open').style.display = 'none';
-    document.getElementById('close').style.display = 'block';
-  };
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  if (open && close) {
+    open.addEventListener('click', event => {
+      openDrawer();
+    });
 
-  closeDrawer = () => {
-    document.getElementById('drawer').style = 'width: 0;';
-    document.getElementById('close').style.display = 'none';
-    document.getElementById('open').style.display = 'block';
-  };
+    close.addEventListener('click', event => {
+      closeDrawer();
+    });
+  }
+};
 
-  window.addEventListener('resize', () => {
-    const drawer = document.getElementById('drawer');
-    if (drawer) {
-      if (this.window.innerWidth > 615) {
-        document.getElementById('drawer').style = 'width: 150px;';
-        document.getElementById('close').style.display = 'none';
-        document.getElementById('open').style.display = 'none';
-      } else {
-        document.getElementById('drawer').style = 'width: 0;';
-        document.getElementById('open').style.display = 'block';
-        document.getElementById('close').style.display = 'none';
-      }
+window.addEventListener('resize', () => {
+  const drawer = document.getElementById('drawer');
+  if (drawer) {
+    if (this.window.innerWidth > 615) {
+      document.getElementById('drawer').style = 'width: 150px;';
+      document.getElementById('close').style.display = 'none';
+      document.getElementById('open').style.display = 'none';
+    } else {
+      document.getElementById('drawer').style = 'width: 0;';
+      document.getElementById('open').style.display = 'block';
+      document.getElementById('close').style.display = 'none';
     }
-  });
+  }
+});
+
+openDrawer = () => {
+  document.getElementById('drawer').style =
+    'width: 150px; position: absolute; top: 10px; z-index: 1;';
+  document.getElementById('open').style.display = 'none';
+  document.getElementById('close').style.display = 'block';
+};
+
+closeDrawer = () => {
+  document.getElementById('drawer').style = 'width: 0;';
+  document.getElementById('close').style.display = 'none';
+  document.getElementById('open').style.display = 'block';
 };
