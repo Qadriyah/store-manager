6f8a7fa708c10f0d76b4c2f8959be7205beb0341
Qadriyah <b.alzawad@gmail.com>
2018-10-17 05:20:01 +0300

    Ch: API environment setup
    This commit adds a sample test case to yield a successful build
    with travis
    [Finishes #161259485]

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,5 +1,29 @@
 from unittest import TestCase
+from datetime import datetime
 
 class TestEndPoints(TestCase):
     def __init__(self):
-        pass
+        self.products = [
+            {
+                "product_id": 1, 
+                "name": "Sugar", 
+                "price": 4500,
+                "created_at": datetime.utcnow
+            }, 
+            {
+                "product_id": 2, 
+                "name": "Bread", 
+                "price": 2700,
+                "created_at": datetime.utcnow
+            }, 
+            {
+                "product_id": 3, 
+                "name": "Milk", 
+                "price": 3000,
+                "created_at": datetime.utcnow
+            }
+        ]
+    
+    def test_product_list(self):
+        """Test if product list is not empty"""
+        self.assertGreater(len(self.products), 0)
