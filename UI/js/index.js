a6d1c1f64523e17c6cb16dd6a940550cec8a40dc
Qadriyah <b.alzawad@gmail.com>
2018-10-17 10:36:02 +0300

    Bg: Remove duplicate javascript code
    This commit removes duplicate javascript code as reported by codeclimate
    [Fixes #161277003]

--- UI/js/index.js
+++ UI/js/index.js
@@ -30,13 +30,9 @@ window.addEventListener('resize', () => {
   const drawer = document.getElementById('drawer');
   if (drawer) {
     if (this.window.innerWidth > 615) {
-      document.getElementById('drawer').style = 'width: 150px;';
-      document.getElementById('close').style.display = 'none';
-      document.getElementById('open').style.display = 'none';
+      openDrawer(3);
     } else {
-      document.getElementById('drawer').style = 'width: 0;';
-      document.getElementById('open').style.display = 'block';
-      document.getElementById('close').style.display = 'none';
+      openDrawer(2);
     }
   }
 });
@@ -47,9 +43,13 @@ openDrawer = selectedOption => {
       'width: 150px; position: absolute; top: 10px; z-index: 1;';
     document.getElementById('open').style.display = 'none';
     document.getElementById('close').style.display = 'block';
-  } else {
+  } else if (selectedOption === 2) {
     document.getElementById('drawer').style = 'width: 0;';
     document.getElementById('close').style.display = 'none';
     document.getElementById('open').style.display = 'block';
+  } else {
+    document.getElementById('drawer').style = 'width: 150px;';
+    document.getElementById('close').style.display = 'none';
+    document.getElementById('open').style.display = 'none';
   }
 };
